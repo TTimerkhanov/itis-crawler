24cb753e4972e7787e51a4f8f0724dc3d3020d88
timurtimerhanov <timurgrunge@gmail.com>
2019-02-23 11:39:25 +0300

    Add Index file generation

--- scrapper/python_site/pipelines.py
+++ scrapper/python_site/pipelines.py
@@ -4,8 +4,6 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-
 import os
 
 from .spiders.python_site import PythonSiteSpider
@@ -15,24 +13,29 @@ class JsonWriterPipeline(object):
     CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
     DATA_PATH = os.path.join(os.path.dirname(CURRENT_PATH), 'data')
 
+    INDEX = {}
+    COUNTER = 0
+
     def get_valid_path(self, path):
-        path = path.replace(PythonSiteSpider.start_urls[0], '')
-        path = path.replace('.php', '.txt')
-        return path.replace('/', '_')
+        valid_path = path.replace(PythonSiteSpider.start_urls[0], '')
+        valid_path = valid_path.replace('.php', '.txt').replace('/', '_')
+        return valid_path
 
-    # def open_spider(self, spider):
-    #     self.file = open('items.json', 'w', encoding='utf-8')
-    #
-    # def close_spider(self, spider):
-    #     self.file.close()
+    def generate_index_file(self):
+        with open(f'{os.path.dirname(self.CURRENT_PATH)}/index.csv', 'w') as wr:
+            wr.write('url,file\n')
+            for url, file in self.INDEX.items():
+                wr.write(f'{url},{file}\n')
 
-    # def process_item(self, item, spider):
-    #     line = json.dumps(dict(item), ensure_ascii=False) + "\n"
-    #     self.file.write(line)
-    #     return item
+    def close_spider(self, spider):
+        self.generate_index_file()
 
     def process_item(self, item, spider):
-        path = self.get_valid_path(item.get("url"))
-        with open(f'{self.DATA_PATH}/{path}', 'w') as wr:
-            wr.write(item.get('text'))
+        if item.get("url") not in self.INDEX.keys():
+            path = f'{self.COUNTER}.txt'
+            self.COUNTER += 1
+            self.INDEX[item.get("url")] = path
+
+            with open(f'{self.DATA_PATH}/{path}', 'w') as wr:
+                wr.write(item.get('text'))
         return item
